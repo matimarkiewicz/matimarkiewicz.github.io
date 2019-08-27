e40f6c16c28ccf9fb420d71ef3db6d6212a78527
matimarkiewicz <mati.markiewicz@gmail.com>
2019-08-27 10:30:41 +0200

    Initial commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'http://localhost:8080/v1/task/';
+  var apiRoot = 'https://shielded-brook-40087.herokuapp.com/v1/task/getTasks';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
